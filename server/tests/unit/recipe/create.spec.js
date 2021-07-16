4795e0bfd02519ce1e708f9cc795c4a4936bd248
padcmoi <juliennaskot@gmail.com>
2021-07-16 13:46:01 +0200

    refactor test unitaire creation de recette

--- server/tests/unit/recipe/create.spec.js
+++ server/tests/unit/recipe/create.spec.js
@@ -1,220 +1,144 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const request = require('supertest')('http://127.0.0.1:3000')
-const { Misc, Db, Password } = require('../../../middleware/index')
-const { randomArray, randomStr } = require('../_misc/index')
+const { Misc, Db, Password, Form } = require('../../../middleware/index')
+const { RecipeManager } = require('../../../constructor/index')
+const {
+  getCsrfToken,
+  SettingManager,
+  FixtureManager,
+  Recipe,
+  goLogin,
+} = require('../_misc/index')
 
-describe('POST /recipe', () => {
-  let csrf_header, fixtures, settings, response, access_token, categories
+describe('POST /recipe/favorite/:slug', () => {
+  let fixtureManager,
+    settingManager,
+    sql_request,
+    csrf_header,
+    origin_settings,
+    fixtures,
+    login_response,
+    access_token,
+    recipe
 
   beforeAll(async () => {
-    csrf_header = await request
-      .get('/csrf/generate')
-      .set({ 'csrf-token': '' })
-      .then((response) => response.body.csrf_token)
-    if (!csrf_header) csrf_header = ''
+    fixtureManager = new FixtureManager()
+    settingManager = new SettingManager()
 
-    let select
+    csrf_header = await getCsrfToken(request)
 
-    // Restaure la table settings au paramètres d'origines
-    select = await Db.get({
-      query: 'SELECT maintenance,can_create_account FROM settings LIMIT 1',
-    })
-    settings = select && select[0]
+    origin_settings = await settingManager.getOriginData()
 
-    await Db.merge({
-      query: 'UPDATE settings SET ? LIMIT 1',
-      preparedStatement: [{ maintenance: 0, can_create_account: 1 }],
-    })
-    // Restaure la table settings au paramètres d'origines
+    await settingManager.setDefault()
 
-    fixtures = {
-      username: '_' + Misc.getRandomStr(15),
-      mail: '_' + Misc.getRandomStr(20) + '@units_tests.na',
-      password: await Password.hash('&_tests_units'),
-      firstname: 'Tests',
-      lastname: 'UNITS',
-      is_lock: 0,
-      jwt_hash: '__tests_units',
-    }
+    fixtures = await fixtureManager.getFixtures()
 
-    select = await Db.get({
-      query: 'SELECT id FROM account WHERE username = ? OR mail = ? LIMIT 1',
-      preparedStatement: [fixtures.username, fixtures.mail],
-    })
+    login_response = await goLogin(request, fixtures.username, '&_tests_units')
 
-    if (!select[0]) {
-      await Db.commit({
-        query: 'INSERT INTO account SET ?',
-        preparedStatement: [fixtures],
-      })
-    }
+    access_token = login_response.access_token || ''
 
-    // Recupère toutes les categories
-    categories = []
-    select = await Db.get({
-      query: 'SELECT category FROM categories',
-    })
-    for (const obj of select) {
-      if (!obj.category) continue
-
-      categories.push(obj.category)
-    }
-    // Recupère toutes les categories
+    recipe = new Recipe(request, access_token)
   })
 
-  it('Login', async (done) => {
-    if (!response) {
-      response = await request
-        .post('/account/login')
-        .set('csrf-token', csrf_header)
-        .send({
-          params: { user: fixtures.username, password: '&_tests_units' },
-        })
-        .then((response) => response.body)
-
-      access_token = response.access_token || ''
-      expect(response.isLoggedIn).toBeTruthy()
+  it('Check login', (done) => {
+    const check = {
+      isLock: 'boolean',
+      isLoggedIn: 'boolean',
+      isAdmin: 'boolean',
+      firstName: 'string',
+      lastName: 'string',
+      toastMessage: 'object',
+      access_token: 'string',
+      execution_time: 'object',
     }
+
+    for (const key in check) expect(typeof login_response[key]).toBe(check[key])
     done()
   })
 
-  describe('fully authenticated', () => {
-    let params,
-      title = randomStr(52)
+  it("Création d'une recette", async (done) => {
+    await recipe.add()
 
-    beforeEach(async () => {
-      params = {
-        title,
-        description: randomStr(60),
-        seasons: randomArray(['Winter', 'Autumn', 'Summer', 'Spring']),
-        difficulty: randomArray(['EaSy', 'MeDIuM', 'hArD'], 1).join(),
-        nutriscore: randomArray(['a', 'B', 'C', 'd', 'e'], 1).join(),
-        preparation_time: Misc.getRandomInt(180, 600),
-        cooking_time: Misc.getRandomInt(300, 600),
-        category: randomArray(categories, 1).join(),
-      }
-    })
+    const response = recipe.getResponse()
+    delete response.execution_time
 
-    it('Recipe successfully created with overfilled form', async (done) => {
-      response = await request
-        .post('/recipe?access_token=' + access_token)
-        .set('csrf-token', csrf_header)
-        .send({
-          params,
-        })
-        .then((response) => response.body)
-
-      expect(response.success).toBeTruthy()
-      expect(response.failProcess.length).toBe(0)
-      expect(response.toastMessage[0].type).toBe('success')
-
-      done()
-    })
-    it('Recipe successfully created with overfilled form', async (done) => {
-      params.title = randomStr(52)
-
-      response = await request
-        .post('/recipe?access_token=' + access_token)
-        .set('csrf-token', csrf_header)
-        .send({
-          params,
-        })
-        .then((response) => response.body)
-
-      expect(response.success).toBeTruthy()
-      expect(response.failProcess.length).toBe(0)
-      expect(response.toastMessage[0].type).toBe('success')
-
-      done()
-    })
-    it('Failed to create recipe with wrong complete form ', async (done) => {
-      params.description = 123
-      params.seasons = 'error'
-      params.difficulty = ['NotArray']
-      params.nutriscore = ['NotArray']
-      params.preparation_time = '300'
-      params.cooking_time = -1
-      params.category = 'Categorie n existe pas'
-
-      response = await request
-        .post('/recipe?access_token=' + access_token)
-        .set('csrf-token', csrf_header)
-        .send({
-          params,
-        })
-        .then((response) => response.body)
-
-      expect(response.success).toBeFalsy()
-      const failProcess = [
-        'slugTitle',
-        'description',
-        'seasons',
-        'difficulty',
-        'nutriscore',
-        'cookingTime',
-        'preparationTime',
-        'category',
-      ]
-      for (let i = 0; i < failProcess.length; i++) {
-        expect(response.failProcess[i]).toBe(failProcess[i])
-      }
-      expect(response.toastMessage.length).toBe(7)
-
-      done()
-    })
+    expect(response.success).toBeTruthy()
+    expect(response.failProcess).toStrictEqual([])
+    expect(response.slug).toBeDefined()
+    expect(response.title).toBeDefined()
+    expect(response.toastMessage.length).toBe(1)
+    done()
   })
+  it('Slug comparaison Api / Function', async (done) => {
+    const data = recipe.getData()
+    const response = recipe.getResponse()
+    delete response.execution_time
 
-  describe('not authenticated', () => {
-    let params,
-      access_token = 'fake token'
+    Form.sanitizeEachData(data, ['seasons'])
 
-    beforeAll(async () => {
-      params = {
-        title: randomStr(52),
-        description: randomStr(60),
-        seasons: randomArray(['Winter', 'Autumn', 'Summer', 'Spring']),
-        difficulty: randomArray(['EaSy', 'MeDIuM', 'hArD'], 1).join(),
-        nutriscore: randomArray(['a', 'B', 'C', 'd', 'e'], 1).join(),
-        preparation_time: Misc.getRandomInt(180, 600),
-        cooking_time: Misc.getRandomInt(300, 600),
-        category: randomArray(categories, 1).join(),
-      }
-    })
+    const required = [
+      'slugTitle',
+      'description',
+      'seasons',
+      'difficulty',
+      'nutriscore',
+      'cookingTime',
+      'preparationTime',
+      'category',
+    ]
 
-    it('Recipe send with overfilled form', async (done) => {
-      response = await request
-        .post('/recipe?access_token=' + access_token)
-        .set('csrf-token', csrf_header)
-        .send({
-          params,
-        })
-        .then((response) => response.body)
+    const recipeManager = new RecipeManager(data, required)
+    const processData = await recipeManager.result()
 
-      expect(response.success).toBeFalsy()
-      expect(response.toastMessage[0].type).toBe('error')
+    expect(response.slug).toStrictEqual(processData.dataProcessed.slug)
+    done()
+  })
+  it('Le titre de la recette existe déja', async (done) => {
+    await recipe.add() // On recrée une recette avec le meme titre
 
-      done()
-    })
+    const response = recipe.getResponse()
+    delete response.execution_time
+
+    expect(response.success).toBeFalsy() // Si la recette existe elle doit etre refusé par l'Api
+    expect(response.failProcess).toStrictEqual(['slugTitle'])
+    expect(response.toastMessage.length).toBe(1)
+    done()
+  })
+  it('Tentative avec un formulaire erroné', async (done) => {
+    await recipe.makeData()
+
+    recipe.params.description = 123
+    recipe.params.seasons = 'error'
+    recipe.params.difficulty = ['NotArray']
+    recipe.params.nutriscore = ['NotArray']
+    recipe.params.preparation_time = '300'
+    recipe.params.cooking_time = -1
+    recipe.params.category = 'Categorie n existe pas'
+
+    await recipe.add() // On recrée une recette avec le meme titre
+
+    const response = recipe.getResponse()
+    delete response.execution_time
+
+    const expected_failProcess = [
+      'description',
+      'seasons',
+      'difficulty',
+      'nutriscore',
+      'cookingTime',
+      'preparationTime',
+      'category',
+    ]
+
+    expect(response.success).toBeFalsy() // Le formulaire contient des erreurs
+    expect(response.failProcess).toStrictEqual(expected_failProcess)
+    expect(response.toastMessage.length).toBe(6)
+    done()
   })
 
   afterAll(async () => {
-    // Restaure la table settings au paramètres administrateurs
-    await Db.merge({
-      query: 'UPDATE settings SET ? LIMIT 1',
-      preparedStatement: [settings],
-    })
-    // Restaure la table settings au paramètres administrateurs
-
-    await Db.delete({
-      query: 'DELETE FROM account WHERE ? LIMIT 1',
-      preparedStatement: { username: fixtures.username },
-    })
-
-    // Restaure l'auto incrémentation
-    await Db.merge({
-      query: 'ALTER TABLE `account` auto_increment = 1;',
-    })
-    // Restaure l'auto incrémentation
+    await settingManager.restoreDefault()
+    await fixtureManager.removeFixtures()
   })
 })
